11699f42ecc73fe8c6a137e4abfc41cab3f3c5d7
Alfiery <alfieryfurlong18@gmail.com>
2025-05-05 02:04:34 -0500

    Inicio crud sucursal

---
+++ src/app/core/models/obtenerSucursal/ObtenerSucursalResponse.ts
@@ -0,0 +1,11 @@
+export interface ObtenerSucursalRequest {
+    id: number,
+    codigoSucursal: string,
+    nombre: string,
+    direccion: string,
+    telefono: string,
+    horaEntrada: string,
+    horaSalida: string,
+    estado: string,
+    responsable: string
+}
--- src/app/private/pages/sucursal/sucursal.component.ts
+++ src/app/private/pages/sucursal/sucursal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sucursal',
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './sucursal.component.html',
   styleUrl: './sucursal.component.scss'
 })
-export class SucursalComponent {
+export class SucursalComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {
+    console.log('SucursalComponent initialized');
+  }
 }
