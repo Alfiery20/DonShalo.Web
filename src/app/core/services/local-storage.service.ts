07a579bf3878f0808ff3c39460e543ab429edfdf
SaidSuyv <sirsuyvi@gmail.com>
2025-06-22 03:09:12 -0500

    update: user data support for jesus

--- src/app/core/services/local-storage.service.ts
+++ src/app/core/services/local-storage.service.ts
@@ -14,7 +14,20 @@ export class LocalStorageService {
   }
 
   getItem(key: string): string | null {
-    return localStorage.getItem(key);
+    const item: string | null = localStorage.getItem(key);
+    if(item == null) return null;
+
+    try{
+      const p = JSON.parse(item);
+      if(typeof p == 'object' && p !== null)
+        return p;
+
+      return item;
+    }
+    catch(e)
+    {
+      return item;
+    }
   }
 
   removeItem(key: string): void {
--- src/app/private/components/modals/user-informacion/user-informacion.component.ts
+++ src/app/private/components/modals/user-informacion/user-informacion.component.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { LocalStorageService } from '../../../../core/services/local-storage.service';
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-user-informacion',
@@ -9,7 +11,14 @@ import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/materia
   templateUrl: './user-informacion.component.html',
   styleUrl: './user-informacion.component.scss'
 })
-export class UserInformacionComponent {
+export class UserInformacionComponent implements OnInit {
   readonly dialogRef = inject(MatDialogRef<UserInformacionComponent>);
-  readonly data = inject<any>(MAT_DIALOG_DATA);
+  readonly localStorageService = inject(LocalStorageService);
+  readonly localStorageKeys = environment.localStorageKeys;
+
+  public userData: any = {};
+
+  ngOnInit(): void {
+    this.userData = this.localStorageService.getItem(this.localStorageKeys.USER);
+  }
 }
